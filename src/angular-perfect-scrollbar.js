210f41503676c49907019747a723cb14ee885605
Adam <Adam.Murray@blackbay.com>
2016-08-05 12:44:41 +1200

    Changes: Add a couple new features to control the scroll postion

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,7 +1,7 @@
 angular.module('perfect_scrollbar', []).directive('perfectScrollbar',
   ['$parse', '$window', function($parse, $window) {
   var psOptions = [
-    'wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'maxScrollbarLength', 'useBothWheelAxes',
+    'wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'useBothWheelAxes',
     'useKeyboard', 'suppressScrollX', 'suppressScrollY', 'scrollXMarginOffset',
     'scrollYMarginOffset', 'includePadding'//, 'onScroll', 'scrollDown'
   ];
@@ -22,32 +22,34 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar',
         }
       }
 
+      obs_opts = {
+        childList: true,
+        subtree: true,
+        characterData: true,
+      };
+
+      if(typeof MutationObserver === "function") {
+        obs = new MutationObserver(function () {
+          $elem.perfectScrollbar('update');
+        });
+        obs.observe($elem[0], obs_opts);
+      } else {
+        console.warn("Your browser does not support MutationObserver");
+      }
+
       $scope.$evalAsync(function() {
         $elem.perfectScrollbar(options);
         var onScrollHandler = $parse($attr.onScroll)
         $elem.scroll(function(){
           var scrollTop = $elem.scrollTop()
           var scrollHeight = $elem.prop('scrollHeight') - $elem.height()
-          var scrollLeft = $elem.scrollLeft()
-          var scrollWidth = $elem.prop('scrollWidth') - $elem.width()
-
           $scope.$apply(function() {
             onScrollHandler($scope, {
               scrollTop: scrollTop,
-              scrollHeight: scrollHeight,
-              scrollLeft: scrollLeft,
-              scrollWidth: scrollWidth
+              scrollHeight: scrollHeight
             })
           })
         });
-      });
-
-      $scope.$watch(function() {
-        return $elem.prop('scrollHeight');
-      }, function(newValue, oldValue) {
-        if (newValue) {
-          update('contentSizeChange');
-        }
       });
 
       function update(event) {
@@ -64,8 +66,40 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar',
       // This is necessary when you don't watch anything with the scrollbar
       $elem.bind('mouseenter', update('mouseenter'));
 
+      $scope.$watch('scrollToTop', function(n,o) {
+        if(n) {
+          $elem.scrollTop(0);
+          $scope.scrollToTop = false;
+          update();
+        }
+      });
+
+      $scope.$watch('scrollToPos', function(n,o) {
+          if(n) {
+
+            // work around if the values are on the scope instead of the controller
+            if(!$scope.vm){
+              $scope.vm = $scope;
+            }
+
+            // move higher or lower to show the item
+            if(($scope.vm.itemPosition - $scope.vm.elementSize) <= $elem.scrollTop()){
+              $elem.scrollTop($scope.vm.itemPosition - $scope.vm.elementSize);
+            }
+            else if (($scope.vm.itemPosition + $scope.vm.elementSize) >= ($elem.scrollTop() + $elem.height() - $scope.vm.elementScrollOffset))
+            {
+              $elem.scrollTop($scope.vm.itemPosition - $scope.vm.elementSize);
+            }
+            // otherwise item should already be in view.
+
+            $scope.scrollToPos = false;
+            update();
+          }
+      });
+
       // Possible future improvement - check the type here and use the appropriate watch for non-arrays
       if ($attr.refreshOnChange) {
+
         $scope.$watchCollection($attr.refreshOnChange, function() {
           update();
         });
